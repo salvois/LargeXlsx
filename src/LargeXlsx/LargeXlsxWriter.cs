e4a52252555a2083c937627f8c0ba229657618d3
Salvo Isaja <salvois@users.sourceforge.net>
2020-01-29 19:29:42 +0100

    Added package info to publish on nuget.org.

--- src/LargeXlsx/LargeXlsxWriter.cs
+++ src/LargeXlsx/LargeXlsxWriter.cs
@@ -1,7 +1,7 @@
 ﻿/*
 LargeXlsx - Minimalistic .net library to write large XLSX files
 
-Copyright 2019 Salvatore ISAJA. All rights reserved.
+Copyright 2020 Salvatore ISAJA. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
