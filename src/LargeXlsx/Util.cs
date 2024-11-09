5dbe091b45e083d1c69dc402526efccac9dbe79a
Mihai Antom <mike101200@outlook.com>
2024-11-09 08:17:52 +0200

    Added aditional removal of invalid xml chars, accepting in the meantime xml surrogate pairs

--- src/LargeXlsx/Util.cs
+++ src/LargeXlsx/Util.cs
@@ -28,87 +28,112 @@ using System;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Xml;
 
 namespace LargeXlsx
 {
-    internal static class Util
-    {
-        private const int MaxColumnCount = 16384;
-        private static readonly DateTime ExcelEpoch = new DateTime(1900, 1, 1);
-        private static readonly DateTime Date19000301 = new DateTime(1900, 3, 1);
-        private static readonly string[] CachedColumnNames = new string[MaxColumnCount];
+	internal static class Util
+	{
+		private const int MaxColumnCount = 16384;
+		private static readonly DateTime ExcelEpoch = new DateTime(1900, 1, 1);
+		private static readonly DateTime Date19000301 = new DateTime(1900, 3, 1);
+		private static readonly string[] CachedColumnNames = new string[MaxColumnCount];
 
-        public static string EscapeXmlText(string value)
-        {
-			return System.Security.SecurityElement.Escape(value);
+		public static string EscapeXmlText(string value)
+		{
+			return System.Security.SecurityElement.Escape(RemoveInvalidXmlChars(value));
 		}
 
-        public static string EscapeXmlAttribute(string value)
-        {
-			return System.Security.SecurityElement.Escape(value);
+		public static string EscapeXmlAttribute(string value)
+		{
+			return System.Security.SecurityElement.Escape(RemoveInvalidXmlChars(value));
 		}
 
-        public static string GetColumnName(int columnIndex)
-        {
-            if (columnIndex < 1 || columnIndex > MaxColumnCount)
-                throw new InvalidOperationException($"A worksheet can contain at most {MaxColumnCount} columns ({columnIndex} attempted)");
-            var columnName = CachedColumnNames[columnIndex - 1];
-            if (columnName == null)
-            {
-                columnName = GetColumnNameInternal(columnIndex);
-                CachedColumnNames[columnIndex - 1] = columnName;
-            }
-            return columnName;
-        }
+		public static string RemoveInvalidXmlChars(string value)
+		{
+			if (string.IsNullOrEmpty(value))
+				return value;
 
-        private static string GetColumnNameInternal(int columnIndex)
-        {
-            var columnName = new StringBuilder(3); // This has been measured to be faster than string concatenation
-            while (true)
-            {
-                if (columnIndex > 26)
-                {
-                    columnIndex = Math.DivRem(columnIndex - 1, 26, out var rem);
-                    columnName.Insert(0, (char)('A' + rem));
-                }
-                else
-                {
-                    columnName.Insert(0, (char)('A' + columnIndex - 1));
-                    return columnName.ToString();
-                }
-            }
-        }
+			var stringBuilrede = new StringBuilder();
 
-        public static double DateToDouble(DateTime date)
-        {
-            var days = date.Subtract(ExcelEpoch).TotalDays + 1;
-            // Excel wrongly assumes that 1900 is a leap year:
-            // https://docs.microsoft.com/en-us/office/troubleshoot/excel/wrongly-assumes-1900-is-leap-year
-            if (date >= Date19000301) days++;
-            return days;
-        }
+			for (int i = 0; i < value.Length; i++)
+			{
+				if (XmlConvert.IsXmlChar(value[i]))
+				{
+					stringBuilrede.Append(value[i]);
+				}
+				else if (char.IsHighSurrogate(value[i]) && i + 1 < value.Length && char.IsLowSurrogate(value[i + 1]))
+				{
+					stringBuilrede.Append(value[i]);
+					stringBuilrede.Append(value[i + 1]);
+					i++;
+				}
+			}
 
-        public static int BoolToInt(bool value) => value ? 1 : 0;
+			return stringBuilrede.ToString();
+		}
 
-        public static string EnumToAttributeValue<T>(T enumValue)
-        {
-            var s = enumValue.ToString();
-            return char.ToLowerInvariant(s[0]) + s.Substring(1);
-        }
+		public static string GetColumnName(int columnIndex)
+		{
+			if (columnIndex < 1 || columnIndex > MaxColumnCount)
+				throw new InvalidOperationException($"A worksheet can contain at most {MaxColumnCount} columns ({columnIndex} attempted)");
+			var columnName = CachedColumnNames[columnIndex - 1];
+			if (columnName == null)
+			{
+				columnName = GetColumnNameInternal(columnIndex);
+				CachedColumnNames[columnIndex - 1] = columnName;
+			}
+			return columnName;
+		}
 
-        // Hashing procedure courtesy of https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-offcrypto/1357ea58-646e-4483-92ef-95d718079d6f
-        public static byte[] ComputePasswordHash(string password, byte[] saltValue, int spinCount)
-        {
-            var hasher = new SHA512Managed();
-            var hash = hasher.ComputeHash(saltValue.Concat(Encoding.Unicode.GetBytes(password)).ToArray());
-            for (var i = 0; i < spinCount; i++)
-            {
-                var iterator = BitConverter.GetBytes(i);
-                if (!BitConverter.IsLittleEndian)
-                    Array.Reverse(iterator);
-                hash = hasher.ComputeHash(hash.Concat(iterator).ToArray());
-            }
-            return hash;
-        }
-    }
+		private static string GetColumnNameInternal(int columnIndex)
+		{
+			var columnName = new StringBuilder(3); // This has been measured to be faster than string concatenation
+			while (true)
+			{
+				if (columnIndex > 26)
+				{
+					columnIndex = Math.DivRem(columnIndex - 1, 26, out var rem);
+					columnName.Insert(0, (char)('A' + rem));
+				}
+				else
+				{
+					columnName.Insert(0, (char)('A' + columnIndex - 1));
+					return columnName.ToString();
+				}
+			}
+		}
+
+		public static double DateToDouble(DateTime date)
+		{
+			var days = date.Subtract(ExcelEpoch).TotalDays + 1;
+			// Excel wrongly assumes that 1900 is a leap year:
+			// https://docs.microsoft.com/en-us/office/troubleshoot/excel/wrongly-assumes-1900-is-leap-year
+			if (date >= Date19000301) days++;
+			return days;
+		}
+
+		public static int BoolToInt(bool value) => value ? 1 : 0;
+
+		public static string EnumToAttributeValue<T>(T enumValue)
+		{
+			var s = enumValue.ToString();
+			return char.ToLowerInvariant(s[0]) + s.Substring(1);
+		}
+
+		// Hashing procedure courtesy of https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-offcrypto/1357ea58-646e-4483-92ef-95d718079d6f
+		public static byte[] ComputePasswordHash(string password, byte[] saltValue, int spinCount)
+		{
+			var hasher = new SHA512Managed();
+			var hash = hasher.ComputeHash(saltValue.Concat(Encoding.Unicode.GetBytes(password)).ToArray());
+			for (var i = 0; i < spinCount; i++)
+			{
+				var iterator = BitConverter.GetBytes(i);
+				if (!BitConverter.IsLittleEndian)
+					Array.Reverse(iterator);
+				hash = hasher.ComputeHash(hash.Concat(iterator).ToArray());
+			}
+			return hash;
+		}
+	}
 }
